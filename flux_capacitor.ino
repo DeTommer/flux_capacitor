650834cdba3bb4448895c2096736a4b441796e7d
DeTommer <31655434+DeTommer@users.noreply.github.com>
2025-07-11 20:27:34 +0200

    updated for use with PlatformIO, needs to be tested!

--- flux_capacitor.ino
+++ flux_capacitor.ino
@@ -26,9 +26,9 @@ void loop() {
     strip1.clear();
     strip2.clear();
     strip3.clear();
-    strip1.setPixelColor(i, strip1.Color(0, 255, 255)); // Cyan
-    strip2.setPixelColor(i, strip2.Color(0, 255, 255));
-    strip3.setPixelColor(i, strip3.Color(0, 255, 255));
+    strip1.setPixelColor(i, strip1.Color(0, 255, 0)); // Green
+    strip2.setPixelColor(i, strip2.Color(0, 255, 0));
+    strip3.setPixelColor(i, strip3.Color(0, 255, 0));
     strip1.show();
     strip2.show();
     strip3.show();
